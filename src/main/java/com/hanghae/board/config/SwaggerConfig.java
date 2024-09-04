a69ae86c82be7e2dbc9158447a32fe792c3fa12e
kilhyeonjun <kboxstar@gmail.com>
2024-09-04 14:59:29 +0900

    feat: SwaggerConfig 추가 (bearer 활성화)

---
+++ src/main/java/com/hanghae/board/config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package com.hanghae.board.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+  @Bean
+  public OpenAPI customOpenAPI() {
+    return new OpenAPI()
+        .addSecurityItem(new SecurityRequirement().addList("JWT"))
+        .components(new Components().addSecuritySchemes("JWT", createAPIKeyScheme()));
+  }
+
+  private SecurityScheme createAPIKeyScheme() {
+    return new SecurityScheme().type(SecurityScheme.Type.HTTP)
+        .bearerFormat("JWT")
+        .scheme("bearer");
+  }
+}
