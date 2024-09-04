075d93e85ae26ed7a99865d9ce2fe812fe71a7f7
kilhyeonjun <kboxstar@gmail.com>
2024-09-04 14:58:30 +0900

    fix: default user role 설정

--- src/main/java/com/hanghae/board/security/UserDetailsServiceImpl.java
+++ src/main/java/com/hanghae/board/security/UserDetailsServiceImpl.java
@@ -4,12 +4,16 @@ import com.hanghae.board.domain.user.entity.User;
 import com.hanghae.board.domain.user.exception.UserErrorCode;
 import com.hanghae.board.domain.user.repository.UserRepository;
 import com.hanghae.board.error.BusinessException;
+import java.util.Collections;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserDetailsServiceImpl implements UserDetailsService {
@@ -22,9 +26,14 @@ public class UserDetailsServiceImpl implements UserDetailsService {
         .orElseThrow(
             () -> new BusinessException(UserErrorCode.USER_NOT_FOUND));
 
+    // TODO: Add roles and authorities
+    SimpleGrantedAuthority authority = new SimpleGrantedAuthority("ROLE_USER");
+    log.info("User {} loaded with default authority: ROLE_USER", username);
+
     return org.springframework.security.core.userdetails.User
         .withUsername(user.getUsername())
         .password(user.getPassword())
+        .authorities(Collections.singletonList(authority))
         .build();
   }
 }
